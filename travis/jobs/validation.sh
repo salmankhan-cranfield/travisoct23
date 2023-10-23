95212f6637a5f0a14f4cdae77298453a59615c5f
Salman Khan <salman.khan1@ibm.com>
2023-10-23 15:52:54 +0100

    added command output

--- travis/jobs/validation.sh
+++ travis/jobs/validation.sh
@@ -5,8 +5,11 @@
 # Documentations related to SFDX CLI can be found here https://developer.salesforce.com/docs/atlas.en-us.sfdx_cli_reference.meta/sfdx_cli_reference/cli_reference.htm
 #
 
+temp=$(mktemp)
 
+sf project deploy validate --source-dir force-app --target-org skprod --wait 120 --test-level RunLocalTests --verbose >> $temp
+echo "Command output:"
+cat "$temp"
+echo "Value of temp variable: $temp"
 
-sf project deploy validate --source-dir force-app --target-org skprod --wait 120 --test-level RunLocalTests --verbose  >> $temp
-echo $temp
 
